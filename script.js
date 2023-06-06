bee39eb43dd2b0deef38a67f0ac702f9d5389529
lalit-rao <lalitmrao@gmail.com>
2023-06-06 22:42:23 +0530

    completed project

---
+++ script.js
@@ -0,0 +1,22 @@
+(function () {
+   'use strict';
+
+    document.getElementById('convert').addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const distance = parseFloat(document.getElementById('distance').value);
+        const answer = document.getElementById('answer');
+
+        // alert(distance);
+        if (distance) {
+            // convert
+            const conversion = (distance * 1.609344).toFixed(3);
+            // display
+            answer.innerHTML = `<h2>${distance} miles converts to ${conversion} kilometers</h2>`
+        }
+        else {
+            // display error
+            answer.innerHTML = '<h2>Please provide a number</h2>'
+        }
+    });
+})()
