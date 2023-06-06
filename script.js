7e45d129276d3168f6192d357aca80bf5d0505b9
lalit-rao <lalitmrao@gmail.com>
2023-06-06 23:27:34 +0530

    completed project

--- script.js
+++ script.js
@@ -16,7 +16,7 @@
         }
         else {
             // display error
-            answer.innerHTML = '<h2>Please provide a number</h2>'
+            answer.innerHTML = '<h2>Please provide a number!</h2>'
         }
     });
 })()
